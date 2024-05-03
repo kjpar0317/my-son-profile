80f5c7df5ee7490e9355027ae64741c03bccc78d
박광주 <kjpar0317@innogrid.com>
2024-05-03 17:38:30 +0900

    minor 패치

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,6 @@
-// import { PrismaClient } from "@prisma/client";
 import { PrismaClient } from "../../prisma/generated/client";
 
-// import { PrismaClient } from "../../prisma/generated/clint/edge";
-// import { PrismaClient } from "@prisma/client/edge";
-// import { withAccelerate } from "@prisma/extension-accelerate";
-
 const prismaClientSingleton = () => {
-  //   return new PrismaClient().$extends(withAccelerate());
   return new PrismaClient();
 };
 
