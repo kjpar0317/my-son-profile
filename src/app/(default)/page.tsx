e29b941eb3873393f414afe4d1df5a4d1e363bbb
kjpar0317 <kjpar0317@gmail.com>
2024-05-05 23:26:35 +0900

    완료

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -4,23 +4,23 @@ import type { ReactElement } from "react";
 
 import { useState, useEffect } from "react";
 import { Chrono } from "react-chrono";
-import useSound from "use-sound";
+// import useSound from "use-sound";
 
 import { convertTimeline } from "@/util/timeline-utils";
 import BgVideo from "@/assets/videos/background.mp4";
-import BgSound from "@/assets/sounds/mistyMoon.mp3";
+// import BgSound from "@/assets/sounds/moonEmbracingTheSun.mp3";
 
 export default function HomePage(): ReactElement {
   const [isClient, setIsClient] = useState(false);
   const [timelines, setTimelines] = useState([]);
-  const [play, { stop }] = useSound(BgSound);
+  // const [play, { stop }] = useSound(BgSound);
 
   useEffect(() => {
-    stop();
-    play();
+    // stop();
+    // play();
     getTimelines();
     setIsClient(true); // Component has mounted, update state to trigger re-render
-  }, [play, stop]);
+  }, []);
 
   async function getTimelines() {
     const res = await fetch(`/api/timeline`);
@@ -29,41 +29,47 @@ export default function HomePage(): ReactElement {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-14">
+    <main className="flex min-h-screen w-full h-full flex-col items-center justify-between pt-10">
       <video
         src={BgVideo}
         autoPlay
         loop
         muted
-        className="w-full h-full object-cover absolute top-0"
+        className="w-full h-screen object-cover absolute top-0"
       />
-      <div className="relative w-full flex">
+      <div className="relative w-11/12 h-[calc(100vh_-_65px)] flex overflow-y-auto">
         {isClient && timelines && (
           <Chrono
             items={convertTimeline(timelines)}
-            mode="HORIZONTAL"
-            cardWidth={800}
-            cardHeight={400}
-            mediaHeight={450}
+            mode="VERTICAL_ALTERNATING"
+            // cardWidth={600}
+            // cardHeight={400}
+            // itemWidth={500}
+            // mediaHeight={450}
             parseDetailsAsHTML
             slideShow
-            slideItemDuration={7000}
+            slideItemDuration={5000}
             highlightCardsOnHover
-            disableInteraction={true}
+            // disableInteraction={true}
             enableLayoutSwitch={false}
-            enableQuickJump={true}
+            enableQuickJump={false}
             allowDynamicUpdate
-            // className="bg-base-200"
+            // showAllCardsHorizontal
+            // scrollable
             mediaSettings={{ align: "right", fit: "cover" }}
+            className="bg-black"
             theme={{
               primary: "#865E92",
               secondary: "#E1FAC5",
               cardBgColor: "#E9E2EB",
               cardForeColor: "#E9E2EB",
-              titleColor: "#9DCA98",
-              titleColorActive: "red",
+              titleColor: "#08ACBC",
+              titleColorActive: "#0A86F3",
               cardTitleColor: "#485639",
               cardSubtitleColor: "#485639",
+            }}
+            classNames={{
+              title: "bg-slate-300",
             }}
             buttonTexts={{
               first: "처음",
